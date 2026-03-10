266ae07068ed50010007e8e8d28d6b7715e37f9c
Chayney <chunchunmaru33333@gmail.com>
2026-03-10 20:20:37 +0900

    バックエンドの構築

--- backend/node/src/index.ts
+++ backend/node/src/index.ts
@@ -3,18 +3,19 @@ import cors from "cors";
 import { authRouter } from "./routes/auth";
 import { todoRouter } from "./routes/todo";
 
-// DB接続設定
-// import { AppDataSource } from "./config/appDataSource";
-
 export const app = express();
 
 export const start = async () => {
-    const PORT = process.env.PORT || 8080;
-    const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+    const PORT =
+        process.env.NODE_ENV === "production"
+            ? process.env.PORT || 8080
+            : 3000;
+    const FRONTEND_URL =
+        process.env.NODE_ENV === "production"
+            ? process.env.FRONTEND_URL
+            : "http://localhost:5173";
 
     try {
-        // await AppDataSource.initialize();
-
         // CORS設定
         app.use(cors({
             origin: FRONTEND_URL,
@@ -31,14 +32,6 @@ export const start = async () => {
             console.log(`✅ Listening on ${PORT}`);
             console.log(`✅ Listening on ${FRONTEND_URL}`);
         });
-
-        // テスト実行時サーバー起動しない
-        // if (process.env.NODE_ENV !== 'test') {
-        //     app.listen(PORT, '0.0.0.0', () => {
-        //         console.log(`✅ Server is running on http://localhost:${PORT}`);
-        //     });
-        // }
-
     } catch (error) {
         console.error('DB connection error', error);
     }
